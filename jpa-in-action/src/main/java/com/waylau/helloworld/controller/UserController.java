e2ae2c308561f6d7e56008d09bd0dc9127c09a8d
jimmylin <mrjimmylin@163.com>
2019-07-27 10:29:07 +0800

    Add

---
+++ jpa-in-action/src/main/java/com/waylau/helloworld/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.waylau.helloworld.controller;
+
+import com.waylau.helloworld.domain.User;
+import com.waylau.helloworld.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.WebParam;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ModelAndView listUser(Model model) {
+        model.addAttribute("userList", userRepository.listUser());
+        model.addAttribute("title", "用户管理");
+        return new ModelAndView("users/list", "userModel", model);
+    }
+
+    @GetMapping("/form")
+    public ModelAndView form(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("title", "创建用户");
+        return new ModelAndView("users/form", "userModel", model);
+    }
+
+    @PostMapping
+    public ModelAndView addUser(User user) {
+        userRepository.saveOrUpdate(user);
+        return new ModelAndView("redirect:/users");
+    }
+
+    @GetMapping("/modify/{id}")
+    public ModelAndView modifyUser(@PathVariable Long id,Model model) {
+        User user = userRepository.findById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("title", "修改用户界面");
+        return new ModelAndView("users/form", "userModel", model);
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteUser(@PathVariable Long id) {
+        userRepository.deleteUser(id);
+        return new ModelAndView("redirect:/users");
+    }
+
+}
